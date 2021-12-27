9f0dfa3db5a1e55ad20c5d69e5c7284e1d8dd5c9
Takashi Suzuki <42316228+k-tokitoh@users.noreply.github.com>
2021-12-27 23:44:06 +0900

    enable rotation

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,6 @@
 import { Matrix, Vector } from "./matrix";
 import { Tetromino, LTetromino, OTetromino } from "./tetromino";
+import cloneDeep from "lodash/cloneDeep";
 
 const main = () => {
   const canvas = document.getElementById("canvas") as HTMLCanvasElement;
@@ -9,22 +10,46 @@ const main = () => {
     return;
   }
   const board = new Board(ctx);
-  setInterval(board.tick, 500);
+  setInterval(board.tick, 300);
 };
 
 window.onload = () => main();
 
+class Hand {
+  constructor() {
+    const tetrominoClasses = [LTetromino, OTetromino];
+    const tetrominoClass =
+      tetrominoClasses[Math.floor(Math.random() * tetrominoClasses.length)];
+    this.tetromino = new tetrominoClass();
+    this.offset = new Vector(5, 0);
+  }
+  tetromino: Tetromino;
+  offset: Vector;
+}
+
 class Board {
   constructor(private readonly _ctx: CanvasRenderingContext2D) {
     this._backgroundLayer = this.getBaseMatrix();
-    this._currentLayer = this.getBaseMatrix();
-    this.current = this.resetCurrent();
+    this.currentHand = new Hand();
+    this.aheadHand = cloneDeep(this.currentHand);
     document.addEventListener("keydown", (e) => this.onKeyDown(e));
   }
 
   private _backgroundLayer: Matrix;
-  private _currentLayer: Matrix;
-  private current: { tetromino: Tetromino; offset: Vector };
+  private currentHand: Hand;
+  private aheadHand: Hand;
+
+  // TODO: cache
+  private get _currentLayer(): Matrix {
+    const matrix = this.getBaseMatrix();
+    this.currentHand.tetromino.posture.cells
+      .filter((cell) => cell.value)
+      .forEach(
+        (cell) =>
+          (matrix.at(cell.vector.add(this.currentHand.offset)).value = 1)
+      );
+    return matrix;
+  }
 
   private getBaseMatrix = () =>
     new Matrix([
@@ -56,80 +81,80 @@ class Board {
       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     ]);
 
-  private resetCurrent() {
-    const tetrominoClasses = [LTetromino, OTetromino];
-    const tetrominoClass =
-      tetrominoClasses[Math.floor(Math.random() * tetrominoClasses.length)];
-    // const tetrominoClass = LTetromino;
-    return {
-      tetromino: new tetrominoClass(),
-      offset: new Vector(5, 0),
-    };
+  private onKeyDown = (e: KeyboardEvent): void => {
+    this.moveByKey(e.key) && this.draw();
+  };
+
+  private moveByKey(key: string) {
+    switch (key) {
+      case "ArrowRight":
+        return this.moveOffset({
+          forward: new Vector(1, 0),
+          backward: new Vector(-1, 0),
+        });
+      case "ArrowLeft":
+        return this.moveOffset({
+          forward: new Vector(-1, 0),
+          backward: new Vector(1, 0),
+        });
+      case "ArrowUp":
+        return this.move({
+          forward: (hand) => hand.tetromino.rotate(1),
+          backward: (hand) => hand.tetromino.rotate(-1),
+        });
+      default:
+        return false;
+    }
   }
 
-  private onKeyDown = (e: KeyboardEvent): void => {
-    const vector = (() => {
-      switch (e.key) {
-        case "ArrowRight":
-          return new Vector(1, 0);
-        case "ArrowLeft":
-          return new Vector(-1, 0);
-      }
-    })();
-    if (!vector) return;
-    this.move({ vector: vector });
-    this.draw();
-  };
+  moveOffset(vectors: { forward: Vector; backward: Vector }) {
+    return this.move({
+      forward: (hand) => (hand.offset = hand.offset.add(vectors.forward)),
+      backward: (hand) => (hand.offset = hand.offset.add(vectors.backward)),
+    });
+  }
 
   tick = () => {
     this.move({
-      vector: new Vector(0, 1),
+      forward: (hand) => (hand.offset = hand.offset.add(new Vector(0, 1))),
+      backward: (hand) => (hand.offset = hand.offset.add(new Vector(0, -1))),
       onCollision: () => {
         this._currentLayer.cells
           .filter((cell) => cell.value)
           .forEach((cell) => (this._backgroundLayer.at(cell.vector).value = 1));
-        this._currentLayer = this.getBaseMatrix();
-        this.current = this.resetCurrent();
+        this.currentHand = new Hand();
+        this.aheadHand = cloneDeep(this.currentHand);
       },
     });
     this.draw();
   };
 
   private move({
-    vector,
+    forward,
+    backward,
     onCollision = () => {},
   }: {
-    vector: Vector;
+    forward: (hand: Hand) => void;
+    backward: (hand: Hand) => void;
     onCollision?: () => void;
   }) {
-    if (this.collisionDetected(vector)) {
+    forward(this.aheadHand);
+    if (this.collisionDetected()) {
+      backward(this.aheadHand);
       onCollision();
-      return;
+      return false;
     }
 
-    this.current.tetromino.posture.cells
-      .filter((cell) => cell.value)
-      .forEach((cell) => {
-        this._currentLayer.at(this.current.offset.add(cell.vector)).value = 0;
-      });
-
-    this.current.offset = this.current.offset.add(vector);
-
-    this.current.tetromino.posture.cells
-      .filter((cell) => cell.value)
-      .forEach((cell) => {
-        this._currentLayer.at(this.current.offset.add(cell.vector)).value = 1;
-      });
+    forward(this.currentHand);
+    return true;
   }
 
-  private collisionDetected(vector: Vector) {
-    return this.current.tetromino.posture.cells
+  private collisionDetected() {
+    return this.aheadHand.tetromino.posture.cells
       .filter((cell) => cell.value)
       .some(
         (cell) =>
-          this._backgroundLayer.at(
-            this.current.offset.add(cell.vector).add(vector)
-          ).value
+          this._backgroundLayer.at(this.aheadHand.offset.add(cell.vector)).value
       );
   }
 
--- src/ts/tetromino.ts
+++ src/ts/tetromino.ts
@@ -5,13 +5,23 @@ export abstract class Tetromino {
     this.postures = this.getRawPostures().map(
       (raw_posture) => new Matrix(raw_posture)
     );
-    this.posture = this.postures[0];
+    this.postureIndex = 0;
   }
 
   private postures: Matrix[];
-  posture: Matrix;
+  private postureIndex: number;
+
+  get posture() {
+    return this.postures[this.postureIndex];
+  }
 
   abstract getRawPostures(): CellValue[][][];
+
+  rotate(indexDiff: number = 1) {
+    this.postureIndex =
+      (this.postureIndex + indexDiff + this.postures.length) %
+      this.postures.length;
+  }
 }
 
 export class LTetromino extends Tetromino {
